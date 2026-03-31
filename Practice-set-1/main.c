b427345af9b7f9f4e94e6f62178a7c310a2b8401
Abdul-Saidat <yewandeabdul822@gmail.com>
2026-03-31 11:58:03 +0100

    completed problem set 1/2

---
+++ Practice-set-1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+// Write a C program to calculate the area of a rectangle:
+// using hard coded inputs, using inputs supplied by the user
+
+// area using hard coded inputs
+int main()
+{
+    int length = 5, breadth = 6;
+    int area = length * breadth;
+    printf("Area of the rectangle is %d", area);
+    return 0;
+}
+
+// area using inputs supplied by the user
